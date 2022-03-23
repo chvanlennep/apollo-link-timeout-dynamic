b9bf2d5d7f7a82287e73ef1f14cbff82010db1ad
Charles van Lennep <charles@eolasmedical.com>
2022-03-23 16:38:27 +0000

    First commit

---
+++ __tests__/timeoutLink.test.ts
@@ -0,0 +1,99 @@
+import TimeoutLink from "../src/timeoutLink";
+import { ApolloLink, execute, Observable } from "@apollo/client/core";
+import gql from "graphql-tag";
+
+const TEST_TIMEOUT = 100;
+
+const timeoutLink: TimeoutLink = new TimeoutLink({ timeout: TEST_TIMEOUT });
+
+const query = gql`
+  {
+    foo {
+      bar
+    }
+  }
+`;
+
+let called, delay;
+
+const mockLink = new ApolloLink((operation) => {
+  called++;
+  return new Observable((observer) => {
+    setTimeout(() => {
+      observer.next({});
+      observer.complete();
+    }, delay);
+  });
+});
+
+const link = timeoutLink.concat(mockLink);
+
+beforeEach(() => {
+  called = 0;
+});
+
+test("short request does not timeout", (done) => {
+  delay = 50;
+
+  execute(link, { query }).subscribe({
+    next() {
+      expect(called).toBe(1);
+      done();
+    },
+    error() {
+      expect("error called").toBeFalsy();
+      done();
+    },
+  });
+});
+
+test("long request times out", (done) => {
+  delay = 200;
+
+  execute(link, { query }).subscribe({
+    next() {
+      expect("next called").toBeFalsy();
+      done();
+    },
+    error(error) {
+      expect(error.message).toEqual("Timeout exceeded");
+      expect(error.timeout).toEqual(100);
+      expect(error.statusCode).toEqual(408);
+      done();
+    },
+  });
+});
+
+test("configured value through context does not time out", (done) => {
+  delay = 200;
+  const configured = 500;
+
+  execute(link, { query, context: { timeout: configured } }).subscribe({
+    next() {
+      expect(called).toBe(1);
+      done();
+    },
+    error(error) {
+      expect("error called").toBeFalsy();
+      done();
+    },
+  });
+});
+
+test("configured short value through context time out", (done) => {
+  delay = 200;
+  const configured = 100;
+
+  execute(link, { query, context: { timeout: configured } }).subscribe({
+    next() {
+      expect("next called").toBeFalsy();
+      done();
+    },
+    error(error) {
+      expect(error.message).toEqual("Timeout exceeded");
+      expect(error.timeout).toEqual(configured);
+      expect(error.statusCode).toEqual(408);
+      done();
+    },
+  });
+});
---
+++ src/timeoutLink.ts
@@ -0,0 +1,134 @@
+import {
+  ApolloLink,
+  Observable,
+  Operation,
+  NextLink,
+} from "@apollo/client/core";
+import { DefinitionNode } from "graphql";
+import TimeoutError from "./TimeoutError";
+
+const DEFAULT_TIMEOUT: number = 15000;
+
+type TimeoutArgs = {
+  timeout: number;
+  statusCode?: number;
+  disableTimeoutCallback?: () => boolean;
+};
+
+/**
+ * Aborts the request if the timeout expires before the response is received.
+ */
+export default class TimeoutLink extends ApolloLink {
+  private timeout: number;
+  private statusCode?: number;
+  private disableTimeoutCallback?: () => boolean;
+
+  constructor({ timeout, statusCode, disableTimeoutCallback }: TimeoutArgs) {
+    super();
+    this.timeout = timeout || DEFAULT_TIMEOUT;
+    this.statusCode = statusCode;
+    this.disableTimeoutCallback = disableTimeoutCallback;
+  }
+
+  public request(operation: Operation, forward: NextLink) {
+    let controller: AbortController;
+
+    // override timeout from query context
+    const requestTimeout = operation.getContext().timeout || this.timeout;
+
+    // add abort controller and signal object to fetchOptions if they don't already exist
+    if (typeof AbortController !== "undefined") {
+      const context = operation.getContext();
+      let fetchOptions = context.fetchOptions || {};
+
+      controller = fetchOptions.controller || new AbortController();
+
+      fetchOptions = { ...fetchOptions, controller, signal: controller.signal };
+      operation.setContext({ fetchOptions });
+    }
+
+    const chainObservable = forward(operation); // observable for remaining link chain
+
+    const operationType = (operation.query.definitions as any).find(
+      (def: DefinitionNode) => def.kind === "OperationDefinition",
+    ).operation;
+
+    let disableTimeout = false;
+
+    if (this.disableTimeoutCallback) {
+      disableTimeout = this.disableTimeoutCallback();
+    }
+
+    if (
+      disableTimeout ||
+      requestTimeout <= 0 ||
+      operationType === "subscription"
+    ) {
+      return chainObservable; // skip this link if timeout is zero or it's a subscription request
+    }
+
+    // create local observable with timeout functionality (unsubscibe from chain observable and
+    // return an error if the timeout expires before chain observable resolves)
+    const localObservable = new Observable((observer) => {
+      let timer: any;
+
+      // listen to chainObservable for result and pass to localObservable if received before timeout
+      const subscription = chainObservable.subscribe(
+        (result) => {
+          clearTimeout(timer);
+          observer.next(result);
+          observer.complete();
+        },
+        (error) => {
+          clearTimeout(timer);
+          observer.error(error);
+          observer.complete();
+        },
+      );
+
+      // if timeout expires before observable completes, abort call, unsubscribe, and return error
+      timer = setTimeout(() => {
+        if (controller) {
+          controller.abort(); // abort fetch operation
+
+          // if the AbortController in the operation context is one we created,
+          // it's now "used up", so we need to remove it to avoid blocking any
+          // future retry of the operation.
+          const context = operation.getContext();
+          let fetchOptions = context.fetchOptions || {};
+          if (
+            fetchOptions.controller === controller &&
+            fetchOptions.signal === controller.signal
+          ) {
+            fetchOptions = { ...fetchOptions, controller: null, signal: null };
+            operation.setContext({ fetchOptions });
+          }
+        }
+
+        observer.error(
+          new TimeoutError("Timeout exceeded", requestTimeout, this.statusCode),
+        );
+        subscription.unsubscribe();
+      }, requestTimeout);
+
+      let ctxRef = operation.getContext().timeoutRef;
+
+      if (ctxRef) {
+        ctxRef({
+          unsubscribe: () => {
+            clearTimeout(timer);
+            subscription.unsubscribe();
+          },
+        });
+      }
+
+      // this function is called when a client unsubscribes from localObservable
+      return () => {
+        clearTimeout(timer);
+        subscription.unsubscribe();
+      };
+    });
+
+    return localObservable;
+  }
+}
